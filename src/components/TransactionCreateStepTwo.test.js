af9a206933fa12cf17565136419ccedd55ab759c
Rehan41302 <saeedrehan3@gmail.com>
2022-08-03 16:16:38 +0500

    test: transformed 2 unit tests to one integration test

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -2,14 +2,10 @@ import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo"
 
-test('on initial render, the pay button is diabled', async () => {
+test('if an amount and note is entered, the pay button becomes enabled', async () => {
     render(<TransactionCreateStepTwo sender={{ id: '9' }} receiver={{ id: '3' }} />);
 
     expect(await screen.findByRole('button', { name: /pay/i })).toBeDisabled()
-})
-
-test('if an amount and note is entered, the pay button becomes enabled', async () => {
-    render(<TransactionCreateStepTwo sender={{ id: '9' }} receiver={{ id: '3' }} />);
 
     userEvent.type(screen.getByPlaceholderText(/amount/i), "50");
     userEvent.type(screen.getByPlaceholderText(/add a note/i), "apple");
