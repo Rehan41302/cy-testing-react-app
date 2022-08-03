b230df7553893c1335d48da56edc7fadba6a977a
Rehan41302 <saeedrehan3@gmail.com>
2022-08-03 16:06:36 +0500

    test: unit test to enable pay button after filling the text inputs

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo"
 
 test('on initial render, the pay button is diabled', async () => {
     render(<TransactionCreateStepTwo sender={{ id: '9' }} receiver={{ id: '3' }} />);
 
     expect(await screen.findByRole('button', { name: /pay/i })).toBeDisabled()
+})
+
+test('if an amount and note is entered, the pay button becomes enabled', async () => {
+    render(<TransactionCreateStepTwo sender={{ id: '9' }} receiver={{ id: '3' }} />);
+
+    userEvent.type(screen.getByPlaceholderText(/amount/i), "50");
+    userEvent.type(screen.getByPlaceholderText(/add a note/i), "apple");
+
+    expect(await screen.findByRole('button', { name: /pay/i })).toBeEnabled()
 })
